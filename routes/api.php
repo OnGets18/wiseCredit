bcc8b25771c604450cdaf39ef9574cb38157ea28
Gets <on@compie.co.il>
2019-01-31 14:52:40 +0200

    First commit

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@
 Route::post('/create', 'UserController@create');
 Route::post('/login', 'UserController@login');
 
-//Request with token. (authUser -> Middleware [go to .../app/Http/Middleware/AuthUser.php])
+//Request with token. (authUser -> Middleware [go to .../app/Http/cre/AuthUser.php])
 Route::middleware(['authUser'])->group(function () {
     Route::get('/account', 'UserController@account');
     Route::patch('/update', 'UserController@update');
